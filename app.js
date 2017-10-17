7e36e34b7d0f309c8e3c68f6990522a8ea3f1bf2
Jacob Schmocker <jacobcs1996@gmail.com>
2017-10-16 20:18:11 -0500

    Basic functionality

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 
-var app = express();
+const index = require('./routes/index');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,12 +31,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.log(err.message);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { Telegram } = require('telegraf');
+const TG = new Telegram(process.env.BOT_TOKEN);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('hello');
+const emailRegExp =
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+router.post('/contact', (req, res) => {
+    console.log(req.body);
+    if (!req.body.name || !req.body.email || !req.body.message) {
+        return res.send('Please fill out all fields.');
+    }
+    if (!emailRegExp.test(req.body.email)) {
+        return res.send('Invalid email address');
+    }
+
+    const message = 
+    `
+*From*: ${req.body.name}  
+*Email*: ${req.body.email}  
+          
+${req.body.message}
+    `;
+
+    TG.sendMessage(process.env.USER_ID, message, {parse_mode: 'Markdown'});
+
+    res.send('success');
 });
 
 module.exports = router;
