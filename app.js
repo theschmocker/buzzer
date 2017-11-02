65354fff02af9b0ba8a2b621e196e93813cb62ea
Jacob Schmocker <jacobcs1996@gmail.com>
2017-11-01 23:40:47 -0500

    cors works

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 // app.use(cookieParser());
 
 app.use('/', index);
